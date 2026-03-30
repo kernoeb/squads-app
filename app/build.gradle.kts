ef5f502a942ca6ce525c544d1498288de7fc58b6
kernoeb <kernoeb@protonmail.com>
2026-03-30 09:19:03 +0200

    bump version to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.squads.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
