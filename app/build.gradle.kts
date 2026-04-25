3a52325a30a0f23341ff7e0e7dbd68e1f9594678
kernoeb <kernoeb@protonmail.com>
2026-04-25 15:33:55 +0200

    bump version to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.squads.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.0"
+        versionCode = 8
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
